ec72f86aabd7a5741b1f85c41b7cab5498d8f6f7
xlibnui_makix <103821356+ZastavnyeMishaUwU@users.noreply.github.com>
2024-10-31 21:19:21 +0200

    Add project files.

---
+++ dz31_10-1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace dz31_10_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть речення: ");
+            string input = Console.ReadLine();
+            string abbreviation = "";
+            string[] words = input.Split(' ');
+            foreach (string word in words)
+            {
+
+                if (!string.IsNullOrEmpty(word))
+                {
+
+                    abbreviation += char.ToUpper(word[0]);
+                }
+            }
+
+
+            Console.WriteLine("Скорочення: " + abbreviation);
+        }
+    }
+}
